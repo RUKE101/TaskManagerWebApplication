82f9b5def9c38d35ddab9ceeffe3732c0d4b24e6
Vasyl <vasiliykub1@gmail.com>
2025-07-13 14:39:52 +0300

    Small interface adding

--- src/main/java/su/taskmanager/data/workspace/repository/ObjectiveRepository.java
+++ src/main/java/su/taskmanager/data/workspace/repository/ObjectiveRepository.java
@@ -4,6 +4,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import su.taskmanager.data.workspace.entity.Objective;
 
+import java.util.Optional;
+
 @Repository
 public interface ObjectiveRepository extends JpaRepository<Objective, Long> {
+    Optional<Objective> findObjectiveById(Long id);
 }
